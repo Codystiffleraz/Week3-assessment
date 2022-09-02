4e2ba623885a6cf5d988930295493f9b03fb7a42
unknown <codystiffleraz@gmail.com>
2022-09-02 08:56:03 -0700

    first a little late

---
+++ about.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('Successfully Submitted');
+}
+
+function catCompliment(evt){
+	evt.preventDefault()
+
+	alert('You\'re an awesome person)
+}
+
+let form = document.querySelector('#contact');
+let pic = document.querySelector('img')
+
+form.addEventListener('submit', handleSubmit);
+pic.addEventListener('mouseover', catCompliment)
